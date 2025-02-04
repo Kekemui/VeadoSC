4f8ac366cce5b5ba31086ce942524100cd7c6e82
Kekemui <kekemui@kekemui.gg>
2025-02-03 22:18:08 -0800

    feat(actions)(#4): add on-hold toggle

---
+++ actions/toggle_state.py
@@ -0,0 +1,32 @@
+from loguru import logger as log  # noqa: F401
+
+from ..messages import (
+    ToggleStateRequest,
+)
+from .action_bases import StateActionBase
+from ..utils import constants
+
+
+class ToggleState(StateActionBase):
+
+    action_id = f"{constants.REV_DNS}::ToggleState"
+    action_name = "Toggle State"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def on_key_down(self):
+        self.toggle()
+
+    def on_key_up(self):
+        self.toggle()
+
+    def toggle(self):
+        success = self.plugin_base.send_request(ToggleStateRequest(self.state_id))
+
+        if not success:
+            self.show_error(5)
+
+    def render(self):
+        self.set_top_label("Toggle", update=False)
+        super().render()
